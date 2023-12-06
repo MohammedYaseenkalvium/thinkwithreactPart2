285779ad64b57dc4e4be38f4981e4f6202fc2b02
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2023-12-06 16:53:37 +0530

    hello

---
+++ app.js
@@ -0,0 +1,13 @@
+class ReactContainer extends React.Component{
+    
+      render(){
+          return (
+          <div>Hello! Welcome to Kalvium   
+             <div>Let's rock and roll - This is babel</div>
+         </div> )
+      };  
+  }
+
+const container = document.getElementById('react-container');
+
+ReactDOM.render(<ReactContainer/>,container);
